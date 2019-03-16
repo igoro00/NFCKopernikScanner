9de77ef8da1362a839e6adf634024351c3febd11
igoro00pl <igor.ordecha@gmail.com>
2019-03-16 20:00:58 +0100

    16.03.2019 0.2: -dodano torun -dodano krakow -zmieniono domyslne miasto na torun

---
+++ app/src/main/java/com/example/nfckopernikscanner/cKrakow.java
@@ -0,0 +1,25 @@
+package com.example.nfckopernikscanner;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class cKrakow extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_c_krakow);
+    }
+    public void toFrombork() {
+        Intent myIntent = new Intent(cKrakow.this, cFrombork.class);
+        //myIntent.putExtra("key", value); //Optional parameters
+        cKrakow.this.startActivity(myIntent);
+    }
+
+
+    public void btnClicked(View view) {
+        toFrombork();
+    }
+}
--- app/src/main/java/com/example/nfckopernikscanner/cTorun.java
+++ app/src/main/java/com/example/nfckopernikscanner/cTorun.java
@@ -1,7 +1,9 @@
 package com.example.nfckopernikscanner;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class cTorun extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class cTorun extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_c_torun);
+    }
+    public void toFrombork() {
+        Intent myIntent = new Intent(cTorun.this, cKrakow.class);
+        //myIntent.putExtra("key", value); //Optional parameters
+        cTorun.this.startActivity(myIntent);
+    }
+
+
+    public void btnClicked(View view) {
+        toFrombork();
     }
 }
