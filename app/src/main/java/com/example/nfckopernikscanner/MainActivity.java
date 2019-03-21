b35064124a746e5f1e37cf178766918dcc71bf14
igoro00pl <igor.ordecha@gmail.com>
2019-03-21 23:58:53 +0100

    16.03.2019 0.35: -rozpoczeto prace nad wsparciem dla BT

--- app/src/main/java/com/example/nfckopernikscanner/MainActivity.java
+++ app/src/main/java/com/example/nfckopernikscanner/MainActivity.java
@@ -1,24 +1,51 @@
 package com.example.nfckopernikscanner;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Set;
+import java.util.UUID;
 
 //ToDo: Miasta w ktorych mieszkal kopernik: Torun, Krakow, Padwa, Olsztyn, Lidzbark Warminski, Frombork
 //ToDo: Wysylanie i odbieranie sygnalow z LEGO Mindstorms EV3
 
 public class MainActivity extends AppCompatActivity {
+    TextView myLabel;
+    BluetoothAdapter mBluetoothAdapter;
+    BluetoothSocket mmSocket;
+    BluetoothDevice mmDevice;
+    OutputStream mmOutputStream;
+    InputStream mmInputStream;
+    Thread workerThread;
+    byte[] readBuffer;
+    int readBufferPosition;
+    int counter;
+    volatile boolean stopWorker;
     private NfcAdapter nfcAdapter;
     byte[] kartaMiejska = {91, 66, 64, 99, 54, 101, 101, 48, 51, 57};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myLabel = (TextView)findViewById(R.id.label);
+        findBT();
+        openBT();
         nfcAdapter = NfcAdapter.getDefaultAdapter(this);
         if (nfcAdapter == null) {
             Toast.makeText(this,
@@ -55,6 +82,11 @@ public class MainActivity extends AppCompatActivity {
                             tagId.toString(),
                             Toast.LENGTH_LONG).show();
                     finish();
+                    try {
+                        sendData("ok");
+                    } catch (IOException ex) {
+                        showMessage("SEND FAILED");
+                    }
                 }
             }
         }
@@ -65,29 +97,136 @@ public class MainActivity extends AppCompatActivity {
         Intent myIntent = new Intent(MainActivity.this, cTorun.class);
         MainActivity.this.startActivity(myIntent);
     }
+
     public void toKrakow() {
         Intent myIntent = new Intent(MainActivity.this, cTorun.class);
         MainActivity.this.startActivity(myIntent);
     }
+
     public void toPadwa() {
         Intent myIntent = new Intent(MainActivity.this, cTorun.class);
         MainActivity.this.startActivity(myIntent);
     }
+
     public void toOlsztyn() {
         Intent myIntent = new Intent(MainActivity.this, cTorun.class);
         MainActivity.this.startActivity(myIntent);
     }
+
     public void toLidzbark() {
         Intent myIntent = new Intent(MainActivity.this, cTorun.class);
         MainActivity.this.startActivity(myIntent);
     }
+
     public void toFrombork() {
         Intent myIntent = new Intent(MainActivity.this, cTorun.class);
         MainActivity.this.startActivity(myIntent);
     }
-
-
     public void btnClicked(View view) {
         toTorun();
+    }
+
+    void findBT() {
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if(mBluetoothAdapter == null) {
+            myLabel.setText("No bluetooth adapter available");
+        }
+
+        if(!mBluetoothAdapter.isEnabled()) {
+            Intent enableBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBluetooth, 0);
+        }
+
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+        if(pairedDevices.size() > 0) {
+            for(BluetoothDevice device : pairedDevices) {
+                if(device.getName().equals("FireFly-108B")) {
+                    mmDevice = device;
+                    break;
+                }
+            }
+        }
+        myLabel.setText("Bluetooth Device Found");
+    }
+
+    void openBT() {
+        try{
+            UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb"); //Standard //SerialPortService ID
+            mmSocket = mmDevice.createRfcommSocketToServiceRecord(uuid);
+            mmSocket.connect();
+            mmOutputStream = mmSocket.getOutputStream();
+            mmInputStream = mmSocket.getInputStream();
+            beginListenForData();
+            myLabel.setText("Bluetooth Opened");
+        }
+        catch(java.io.IOException e) {
+            Log.e("openBT", "opening BT failed" + e);
+        }
+    }
+
+    void beginListenForData() {
+        final Handler handler = new Handler();
+        final byte delimiter = 10; //This is the ASCII code for a newline character
+
+        stopWorker = false;
+        readBufferPosition = 0;
+        readBuffer = new byte[1024];
+        workerThread = new Thread(new Runnable() {
+            public void run() {
+                while(!Thread.currentThread().isInterrupted() && !stopWorker) {
+                    try {
+                        int bytesAvailable = mmInputStream.available();
+                        if(bytesAvailable > 0) {
+                            byte[] packetBytes = new byte[bytesAvailable];
+                            mmInputStream.read(packetBytes);
+                            for(int i=0;i<bytesAvailable;i++) {
+                                byte b = packetBytes[i];
+                                if(b == delimiter) {
+                                    byte[] encodedBytes = new byte[readBufferPosition];
+                                    System.arraycopy(readBuffer, 0, encodedBytes, 0, encodedBytes.length);
+                                    final String data = new String(encodedBytes, "US-ASCII");
+                                    readBufferPosition = 0;
+
+                                    handler.post(new Runnable() {
+                                        public void run() {
+                                            myLabel.setText(data);
+                                        }
+                                    });
+                                }
+                                else {
+                                    readBuffer[readBufferPosition++] = b;
+                                }
+                            }
+                        }
+                    }
+                    catch (IOException ex) {
+                        stopWorker = true;
+                    }
+                }
+            }
+        });
+
+        workerThread.start();
+    }
+
+    void sendData(String msg) throws IOException {
+        msg += "\n";
+        //mmOutputStream.write(msg.getBytes());
+        mmOutputStream.write('A');
+        myLabel.setText("Data Sent");
+    }
+
+    void closeBT() throws IOException {
+        stopWorker = true;
+        mmOutputStream.close();
+        mmInputStream.close();
+        mmSocket.close();
+        myLabel.setText("Bluetooth Closed");
+    }
+
+    private void showMessage(String theMsg) {
+        Toast msg = Toast.makeText(getBaseContext(),
+                theMsg, (Toast.LENGTH_LONG)/160);
+        msg.show();
     }
 }
