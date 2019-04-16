b336a95707d7514a1c9b3c5a31ecc648589c312c
igoro00pl <igor.ordecha@gmail.com>
2019-04-16 14:16:23 +0200

    odchudzenie v2

--- app/src/main/java/com/example/nfckopernikscanner/CityActivity.java
+++ app/src/main/java/com/example/nfckopernikscanner/CityActivity.java
@@ -3,15 +3,53 @@ package com.example.nfckopernikscanner;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
 public class CityActivity extends AppCompatActivity {
-
+    String city;
+    TextView txt;
+    TextView title;
+    ImageView img;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_city);
+        city = getIntent().getStringExtra("EXTRA_CITY");
+        txt = (TextView)findViewById(R.id.textViewINFO);
+        title = (TextView)findViewById(R.id.textViewCITY);
+        img = (ImageView)findViewById(R.id.imageViewC);
+        if(city.equals("olsztyn")){
+            title.setText("OLSZTYN");
+            txt.setText(R.string.olsztyn_info);
+            img.setImageResource(R.drawable.c_olsztyn);
+        }
+        if(city.equals("dobre")){
+            title.setText("DOBRE MIASTO");
+            txt.setText(R.string.dobre_info);
+            img.setImageResource(R.drawable.c_dobre);
+        }
+        if(city.equals("lidzbark")){
+            title.setText("LIDZBARK WARMIŃSKI");
+            txt.setText(R.string.lidzbark_info);
+            img.setImageResource(R.drawable.c_lidzbark);
+        }
+        if(city.equals("pieniezno")){
+            title.setText("PIENIĘŻNO");
+            txt.setText(R.string.pieniezno_info);
+            img.setImageResource(R.drawable.c_pieniezno);
+        }
+        if(city.equals("frombork")){
+            title.setText("FROMBORK");
+            txt.setText(R.string.frombork_info);
+            img.setImageResource(R.drawable.c_frombork);
+        }
+        if(city.equals("torun")){
+            title.setText("TORUŃ");
+            txt.setText(R.string.torun_info);
+            img.setImageResource(R.drawable.c_torun);
+        }
     }
 
 
